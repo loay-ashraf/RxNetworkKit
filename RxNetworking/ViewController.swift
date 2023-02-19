624e420c7234ac8f78a04a63484a30b304dad04a
Loay Ashraf <loay.ashraf.96@gmail.com>
2023-02-19 06:28:56 +0200

    feat: add network manager class
    Resolves: none.

--- RxNetworking/ViewController.swift
+++ RxNetworking/ViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
 
+    let disposeBag: DisposeBag = .init()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let session = URLSession(configuration: .default)
+        let manager = NetworkManager(session: session)
+        let router = CatFactRouter.fact
+        let maybe: Single<CatFact> = manager.request(router, EmptyNetworkAPIError.self)
+        maybe
+            .subscribe(onSuccess: { debugPrint($0) }, onFailure: { debugPrint($0) })
+            .disposed(by: disposeBag)
     }
-
-
 }
 
