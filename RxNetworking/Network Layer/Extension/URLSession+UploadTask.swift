f72725ed0ed58671223e2363dd6ac09439e703dd
Loay Ashraf <loay.ashraf.96@gmail.com>
2023-03-24 21:39:14 +0200

    refactor: add reusable code in one method
    Resolves: none.

--- RxNetworking/Network Layer/Extension/URLSession+UploadTask.swift
+++ RxNetworking/Network Layer/Extension/URLSession+UploadTask.swift
@@ -10,6 +10,22 @@ import Foundation
 extension URLSession {
     func fileUploadTask(with request: URLRequest, from file: File, completionHandler: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
         var request = request
+        let fileData = extractFileData(file)
+        request.setValue(file.mime.rawValue, forHTTPHeaderField: "Content-Type")
+        request.httpBody = fileData
+        let task = dataTask(with: request, completionHandler: completionHandler)
+        return task
+    }
+    func formDataUploadTask(with request: URLRequest, from formData: FormData, completionHandler: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        var request = request
+        let boundary = generateFormBoundary()
+        let dataBody = createFormDataBody(formData: formData, boundary: boundary)
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        request.httpBody = dataBody
+        let task = dataTask(with: request, completionHandler: completionHandler)
+        return task
+    }
+    private func extractFileData(_ file: File) -> Data? {
         var data: Data? = nil
         if let fileData = file.data {
             data = fileData
@@ -17,19 +33,7 @@ extension URLSession {
                   let fileData = try? Data(contentsOf: fileURL) {
             data = fileData
         }
-        request.setValue(file.mime.rawValue, forHTTPHeaderField: "Content-Type")
-        request.httpBody = data
-        let task = dataTask(with: request, completionHandler: completionHandler)
-        return task
-    }
-    func formDataUploadTask(with request: URLRequest, from formData: FormData, completionHandler: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-        var request = request
-        let boundary = generateFormBoundary()
-        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
-        let dataBody = createFormDataBody(formData: formData, boundary: boundary)
-        request.httpBody = dataBody
-        let task = dataTask(with: request, completionHandler: completionHandler)
-        return task
+        return data
     }
     private func createFormDataBody(formData: FormData, boundary: String) -> Data {
         let files = formData.files
@@ -42,19 +46,11 @@ extension URLSession {
             body.append("\(value + lineBreak)")
         }
         for file in files {
-            var data: Data = .init()
-            if let fileData = file.data {
-                data = fileData
-            } else if let fileURL = file.url,
-                      let fileData = try? Data(contentsOf: fileURL) {
-                data = fileData
-            } else {
-                continue
-            }
+            guard let fileData = extractFileData(file) else { continue }
             body.append("--\(boundary + lineBreak)")
             body.append("Content-Disposition: form-data; name=\"\(file.key)\"; filename=\"\(file.name)\"\(lineBreak)")
             body.append("Content-Type: \(file.mime.rawValue + lineBreak + lineBreak)")
-            body.append(data)
+            body.append(fileData)
             body.append(lineBreak)
         }
         body.append("--\(boundary)--\(lineBreak)")
