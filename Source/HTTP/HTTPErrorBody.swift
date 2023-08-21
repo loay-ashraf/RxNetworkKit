82aaae3fbdb43b8c8afcbf0c579bc273b83e28e7
Loay Ashraf <loay.ashraf.96@gmail.com>
2023-08-21 21:48:03 +0300

    feat: add decoding capability in case of failure caused due to HTTP status code
    Resolves: none.

---
+++ Source/HTTP/HTTPErrorBody.swift
@@ -0,0 +1,10 @@
+//
+//  HTTPErrorBody.swift
+//  RxNetworkKit
+//
+//  Created by Loay Ashraf on 21/08/2023.
+//
+
+import Foundation
+
+public protocol HTTPErrorBody: Decodable { }
--- Source/Rx Extensions/Rx+NetworkOps/Single/Single+VerifyResponse.swift
+++ Source/Rx Extensions/Rx+NetworkOps/Single/Single+VerifyResponse.swift
@@ -13,9 +13,9 @@ extension PrimitiveSequence where Trait == SingleTrait, Element == (response: HT
     /// Verifies response's status code.
     ///
     /// - Returns: `Single` observable to be observed for values.
-    func verifyResponse() -> Single<Element> {
+    func verifyResponse<E: HTTPErrorBody>(_ httpErrorType: E.Type) -> Single<Element> {
         map { (response: HTTPURLResponse, data: Data) in
-            if let networkError = NetworkError.init(response) {
+            if let networkError = NetworkError.init(response, data: data, errorType: E.self) {
                 throw networkError
             } else {
                 return (response, data)
