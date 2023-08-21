74b32daef0bf251d35d890d44dd0ed3509f33212
Loay Ashraf <loay.ashraf.96@gmail.com>
2023-08-22 00:15:51 +0300

    feat: add empt macOS example project to workspace
    Resolves: none.

---
+++ Examples/macOS/macOS Example/AppDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.swift
+//  macOS Example
+//
+//  Created by Loay Ashraf on 22/08/2023.
+//
+
+import Cocoa
+import RxDataSources
+import RxNetworkKit
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Examples/macOS/macOS Example/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  macOS Example
+//
+//  Created by Loay Ashraf on 22/08/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
