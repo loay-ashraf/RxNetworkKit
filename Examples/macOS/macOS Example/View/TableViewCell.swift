330ed10748b313ce12baba0351701dc6fb966f89
Loay Ashraf <loay.ashraf.96@gmail.com>
2023-08-22 19:26:09 +0300

    feat: add files to macOS example directory
    Resolves: none.

---
+++ Examples/macOS/macOS Example/View/TableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  TableViewCell.swift
+//  RxNetworkKit
+//
+//  Created by Loay Ashraf on 01/04/2023.
+//
+#if canImport(UIKit)
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    @IBOutlet weak var customTextLabel: UILabel!
+    @IBOutlet weak var customImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        customImageView.layer.cornerRadius = 50.0
+        customImageView.layer.cornerCurve = .continuous
+    }
+}
+#endif
