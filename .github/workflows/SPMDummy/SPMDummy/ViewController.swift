994e249ab965f01a241a05a543778e75eabb00ac
Loay Ashraf <loay.ashraf.96@gmail.com>
2023-08-25 06:12:58 +0300

    add dummy project to test SPM integration

---
+++ .github/workflows/SPMDummy/SPMDummy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SPMDummy
+//
+//  Created by Loay Ashraf on 25/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
