2ca2a46e7c577e02b912351818ce8fa88da24202
Loay Ashraf <loay.ashraf.96@gmail.com>
2023-08-25 16:02:12 +0300

    v0.0.2 Fix + Add CI Workflows (#34)
    * feat: add decoding capability in case of failure caused due to HTTP status code
    
    Resolves: none.
    
    * feat: override User-Agent HTTP header in session configuration (#26)
    
    Resolves: none.
    
    * Add macOS example (#27)
    
    * feat: move iOS example project into new sub folder
    
    Resolves: none.
    
    * fix: update framework search paths
    
    Resolves: none.
    
    * feat: add empt macOS example project to workspace
    
    Resolves: none.
    
    * fix: update framework scheme name
    
    Resolves: none.
    
    * feat: add files to macOS example directory
    
    Resolves: none.
    
    * fix: apply public access modifier to DefaultHTTPErrorBody and DefaultNetworkAPIError
    
    Resolves: none.
    
    * refactor: add description comment for ProcessInfo extension
    
    Resolves: none.
    
    * fix: add RxNetworkKit bridging header reference
    
    Resolves: none.
    
    * refactor: apply project recommended settings
    
    Resolves: none.
    
    * feat: connect viewModel to tableview UI
    
    Resolves: none.
    
    * feat: complete ViewController class in macOS Example
    
    Resolves: none.
    
    * refactor: remove old un-needed file
    
    Resolves: none.
    
    * fix: apply correct image scale for error view
    
    Resolves: none.
    
    * Apply new version (0.0.2) (#28)
    
    * fix: remove tinted button warning
    
    Resolves: none.
    
    * version: 0.0.2
    
    Resolves: none.
    
    * fix: update version for podSpec file
    
    Resolves: none.
    
    * fix: remove RxDataSources import statement (#31)
    
    Resolves: none.
    
    * Add CI Workflows For Repository (#33)
    
    * feat: add iOS workflow
    
    Resolves: none.
    
    * fix: update build-ios.yml
    
    Resolves: none.
    
    * Update build-ios.yml
    
    * Update build-ios.yml
    
    * Update build-ios.yml
    
    * Update build-ios.yml
    
    * Update build-ios.yml
    
    * Update build-ios.yml
    
    * Update build-ios.yml
    
    * Update build-ios.yml
    
    * Update build-ios.yml
    
    * Update build-ios.yml
    
    * Update build-ios.yml
    
    * Update build-ios.yml
    
    * Update build-ios.yml
    
    * Update build-ios.yml
    
    * Update build-ios.yml
    
    * Update build-ios.yml
    
    * Update build-ios.yml
    
    * Update build-ios.yml
    
    * Update build-ios.yml
    
    * Update build-ios.yml
    
    * Update build-ios.yml
    
    * Update build-ios.yml
    
    * Update build-ios.yml
    
    * Update build-ios.yml
    
    * Create build-macos.yml
    
    * Update build-ios.yml + build-macos.yml
    
    * fix error 65
    
    * Update build-ios.yml
    
    * update build-ios.yml + build-macos.yml
    
    * Update build-macos.yml
    
    * Update project.pbxproj
    
    * add publish-podspec.yml
    
    * add workflow files outside of folders
    
    * Update publish-podspec.yml
    
    * Update publish-podspec.yml
    
    * Update publish-podspec.yml
    
    * Create pod-lib-lint.yml
    
    * Update pod-lib-lint.yml
    
    * Update pod-lib-lint.yml
    
    * Update pod-lib-lint.yml
    
    * Update pod-lib-lint.yml
    
    * Update pod-lib-lint.yml
    
    * test container workflow
    
    * Update build.yml
    
    * Update build.yml
    
    * add some changes
    
    * Update build.yml
    
    * add some changes
    
    * Update build.yml
    
    * refactor: update names + remove comments
    
    Resolves: none.
    
    * add trigger for trunk push workflow
    
    * add dummy project to test SPM integration
    
    * add spm-lint.yml
    
    * Update spm-lint.yml
    
    * Update spm-lint.yml
    
    * Update spm-lint.yml
    
    * Update spm-lint.yml
    
    * update dummy project
    
    * update workspace dependencies versions

---
+++ .github/workflows/SPMDummy/SPMDummy/AppDelegate.swift
@@ -0,0 +1,63 @@
+//
+//  AppDelegate.swift
+//  SPMDummy
+//
+//  Created by Loay Ashraf on 25/08/2023.
+//
+
+#if os(iOS)
+import UIKit
+import RxNetworkKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+#elseif os(macOS)
+import Cocoa
+import RxNetworkKit
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+        NetworkReachability.shared.start()
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+#endif
