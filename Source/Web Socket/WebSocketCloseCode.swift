4795fd78fe9940b1451957d6b07615c68846bddc
Loay Ashraf <loay.ashraf.96@gmail.com>
2023-10-04 22:51:39 +0300

    feat: add WebSocket capability to NetworkManager (#46)
    Resolves: none.

---
+++ Source/Web Socket/WebSocketCloseCode.swift
@@ -0,0 +1,10 @@
+//
+//  WebSocketCloseCode.swift
+//  RxNetworkKit
+//
+//  Created by Loay Ashraf on 04/10/2023.
+//
+
+import Foundation
+
+public typealias WebSocketCloseCode = URLSessionWebSocketTask.CloseCode
